9eb4c5af825df7689883dd3b38ea6531d9aaeed0
Usman Ikram Sheikh <usman.ikram@systemsltd.com>
2017-05-19 02:47:16 -0700

    Add project files.

---
+++ PlayCode/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlayCode.Startup))]
+namespace PlayCode
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
