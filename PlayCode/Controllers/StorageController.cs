48cc997183e1bb94cdb8cbfa29fb02e803134f40
Usman Ikram Sheikh <usman.ikram@systemsltd.com>
2017-05-19 05:22:25 -0700

    WebJobAdded

---
+++ PlayCode/Controllers/StorageController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+using PlayCode.Models;
+
+namespace PlayCode.Controllers
+{
+    public class StorageController : Controller
+    {
+        // GET: Storage
+        public ActionResult Index()
+        {
+            var storageConnectionStirng = ConfigurationManager.ConnectionStrings["StorageCon"].ConnectionString;
+
+            var storageAccount = CloudStorageAccount.Parse(storageConnectionStirng);
+
+            var tableClient = storageAccount.CreateCloudTableClient();
+
+            var table = tableClient.GetTableReference("customer");
+
+            table.CreateIfNotExists();
+
+            var customer = new CustomerEntity(Guid.NewGuid())
+            {
+                FirstName = "Usman",
+                LastName = "Ikram",
+                Email = "usman.ikram@microtechx.org",
+                PhoneNumber = "03215121621"
+
+            };
+
+            var insertOperation = TableOperation.Insert(customer);
+
+            table.Execute(insertOperation);
+
+
+            var queueClient = storageAccount.CreateCloudQueueClient();
+
+            var queue = queueClient.GetQueueReference("customerqueue");
+
+            queue.CreateIfNotExists();
+
+            var message = new CloudQueueMessage(customer.RowKey);
+
+            queue.AddMessage(message);
+            return View(customer);
+        }
+
+        
+    }
+
+    
+}
---
+++ PlayCode/Models/CustomerEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace PlayCode.Models
+{
+    public class CustomerEntity: TableEntity
+    {
+        public CustomerEntity(Guid employeeId)
+        {
+            PartitionKey = "customer";
+            RowKey = employeeId.ToString();
+        }
+
+        public CustomerEntity()
+        {
+            
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
